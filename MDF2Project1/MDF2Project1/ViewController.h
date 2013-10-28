8a80f3e92065e6b186cc111d9752921666a54536
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-10-28 16:01:32 -0600

    Initial commit for project 1 of MDF2 1311 with new single view and new repo

---
+++ MDF2Project1/MDF2Project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MDF2Project1
+//
+//  Created by Elijah Freestone on 10/28/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MDF2Project1/MDF2Project1Tests/MDF2Project1Tests.m
@@ -0,0 +1,34 @@
+//
+//  MDF2Project1Tests.m
+//  MDF2Project1Tests
+//
+//  Created by Elijah Freestone on 10/28/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MDF2Project1Tests : XCTestCase
+
+@end
+
+@implementation MDF2Project1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
