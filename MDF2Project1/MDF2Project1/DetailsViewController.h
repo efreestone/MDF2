e7763f8f077c1652e44e6441e85d192d0cec3440
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-10-29 20:31:48 -0600

    Added and set up detail view with push segue. Currently displays a test array

---
+++ MDF2Project1/MDF2Project1/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  MDF2Project1
+//
+//  Created by Elijah Freestone on 10/29/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ MDF2Project1/MDF2Project1/DetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailsViewController.m
+//  MDF2Project1
+//
+//  Created by Elijah Freestone on 10/29/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
