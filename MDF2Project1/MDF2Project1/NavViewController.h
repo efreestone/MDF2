29b5cc4f00fa7a6b42fa0b9652de621f363546ec
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-10-29 19:20:07 -0600

    Started setting up storyboard w/ nav and table view. Includes basic supporting files and outlets.

---
+++ MDF2Project1/MDF2Project1/NavViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavViewController.h
+//  MDF2Project1
+//
+//  Created by Elijah Freestone on 10/29/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavViewController : UINavigationController
+
+@end
---
+++ MDF2Project1/MDF2Project1/TwitterCell.m
@@ -0,0 +1,29 @@
+//
+//  TwitterCell.m
+//  MDF2Project1
+//
+//  Created by Elijah Freestone on 10/29/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "TwitterCell.h"
+
+@implementation TwitterCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
