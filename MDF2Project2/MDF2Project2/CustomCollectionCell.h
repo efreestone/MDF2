cc751f535c7dfb884a9e2e38de596d7cae1b1019
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-04 22:26:45 -0700

    Set up collection view (first with storyboard, replaced with nibs) and have test data and images displaying

---
+++ MDF2Project2/MDF2Project2/CustomCollectionCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionCell.h
+//  MDF2Project2
+//
+//  Created by Elijah Freestone on 11/4/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionCell : UICollectionViewCell
+
+@end
---
+++ MDF2Project2/MDF2Project2/CustomCollectionCell.m
@@ -0,0 +1,31 @@
+//
+//  CustomCollectionCell.m
+//  MDF2Project2
+//
+//  Created by Elijah Freestone on 11/4/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomCollectionCell.h"
+
+@implementation CustomCollectionCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
