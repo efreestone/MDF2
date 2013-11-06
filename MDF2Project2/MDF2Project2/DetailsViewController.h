af0d65643315e6c84d872f2641e1f1696ac74059
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-05 17:21:58 -0700

    Added detail view with both nibs and tweaked twitter method more

---
+++ MDF2Project2/MDF2Project2/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  MDF2Project2
+//
+//  Created by Elijah Freestone on 11/5/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
---
+++ MDF2Project2/MDF2Project2/DetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailsViewController.m
+//  MDF2Project2
+//
+//  Created by Elijah Freestone on 11/5/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
