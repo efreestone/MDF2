05b0349a540e15b08613f774a0448005a73b16db
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-09 12:50:03 -0700

    Initial commit for Project 3 with new single view using stroyboards as of now.

---
+++ MDF2Project3/MDF2Project3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MDF2Project3
+//
+//  Created by Elijah Freestone on 11/9/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
