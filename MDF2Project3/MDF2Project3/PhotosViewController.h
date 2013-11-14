18324e9a325f4c26c8b199fb4824ccbe645a102f
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-13 19:48:21 -0700

    Added icons and photos view. Moved images and save method to photos view controller

---
+++ MDF2Project3/MDF2Project3/PhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.h
+//  MDF2Project3
+//
+//  Created by Elijah Freestone on 11/13/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UIViewController
+
+@end
---
+++ MDF2Project3/MDF2Project3/PhotosViewController.m
@@ -0,0 +1,38 @@
+//
+//  PhotosViewController.m
+//  MDF2Project3
+//
+//  Created by Elijah Freestone on 11/13/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
