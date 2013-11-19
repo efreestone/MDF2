123a3940e52e5e96d956c951e5cd772813c61ad4
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-19 15:49:41 -0700

    Added custom cell and set up table view delegate/datasource. Working through section header video

---
+++ MDF2Project4/MDF2Project4/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  MDF2Project4
+//
+//  Created by Elijah Freestone on 11/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ MDF2Project4/MDF2Project4/CustomTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.m
+//  MDF2Project4
+//
+//  Created by Elijah Freestone on 11/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
