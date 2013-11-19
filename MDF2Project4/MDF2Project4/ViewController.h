12d99a7d6492e3ee3b7c335a7ee8edab984ab502
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-19 14:22:50 -0700

    Initial commit for project 4. Getting a rather late start on this one...

---
+++ MDF2Project4/MDF2Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MDF2Project4
+//
+//  Created by Elijah Freestone on 11/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
