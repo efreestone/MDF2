48c58803f6ed72c886ea13ba09f7b73dbc39a564
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-19 21:19:43 -0700

    Added detail view controller

---
+++ MDF2Project4/MDF2Project4/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  MDF2Project4
+//
+//  Created by Elijah Freestone on 11/19/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
