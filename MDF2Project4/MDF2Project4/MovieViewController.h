0db4af64c04c683d6159111c0ebcb52809411e7c
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-11-20 19:04:38 -0700

    Added movie view controller

---
+++ MDF2Project4/MDF2Project4/MovieViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieViewController.h
+//  MDF2Project4
+//
+//  Created by Elijah Freestone on 11/20/13.
+//  Copyright (c) 2013 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieViewController : UIViewController
+
+@end
